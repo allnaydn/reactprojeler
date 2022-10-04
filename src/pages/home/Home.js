f8891693a136a7c720d1cca7ba1e23955c7d8439
aleyna <aleynaaydin07@gmail.com>
2022-10-04 20:03:36 +0300

    Harcama Ekleme Formu Oluşturuldu

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,24 @@
  import './Home.module.css'
- 
+ import { Container,Grid,Paper } from '@mui/material'
+ import Form from './Form'
  
  
  
  export default function Home(){
     return(
-        <div>
-         Anasayfa
-        </div>
+        <Container sx ={{mt:8}}>
+            <Grid container spacing={2}>
+                <Grid item md={8} sm={12} xs={12}>
+                 <Paper>Liste</Paper>
+                </Grid>
+
+                <Grid item md={4} sm={12} xs={12}>
+                    <Form/>
+                    </Grid>
+             
+         
+            </Grid>
+        </Container>
     )
  }
 
